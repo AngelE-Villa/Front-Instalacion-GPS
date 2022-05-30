43831be4485024a1994d46b8f018367633d3b80c
ismaelDjC <75846564+ismaelDjC@users.noreply.github.com>
2022-05-30 10:22:41 -0500

    Crear planes

--- src/app/modulos/seleccion-plan/seleccion-plan.component.ts
+++ src/app/modulos/seleccion-plan/seleccion-plan.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
 import {PlanService} from "../../servicios/PlanService";
 import {Plan} from "../../modelos/Plan";
+import { MatDialog } from "@angular/material/dialog";
+import {Modelo} from "../../modelos/Modelo";
+import {Gps} from "../../modelos/Gps";
+import {ModeloService} from "../../servicios/ModeloService";
+
 
 @Component({
   selector: 'app-seleccion-plan',
@@ -9,12 +14,48 @@ import {Plan} from "../../modelos/Plan";
 })
 export class SeleccionPlanComponent implements OnInit {
 
-  constructor(private servicioPlan:PlanService) { }
+
+  constructor(private servicioPlan:PlanService,public dialog: MatDialog,private modeloService: ModeloService) { }
   listaPlanes:Array<Plan>=[];
+
+  listaModelo:Array<Modelo>=[];
+
+  plan:Plan =new Plan();
+
+  modelo:Modelo =new Modelo();
+
+  gps:Gps =new Gps();
+
+  titulo="";
+  editing=false;
+  creating=false;
+
+  @ViewChild('dialogNplan')
+  dialogNplan!: TemplateRef<any>;
 
   ngOnInit(): void {
     this.servicioPlan.getPlanes().subscribe((data:any)=>{
       this.listaPlanes=data;
+    })
+
+    this.modeloService.getModelos().subscribe((data:any)=>{
+      this.listaModelo=data;
+    })
+  }
+
+
+  abrirdialogoPlanes(){
+    this.editing=false;
+    this.creating=true;
+    this.titulo="Crear Plan"
+    this.dialog.open(this.dialogNplan);
+  }
+
+
+  GuardarPlan(){
+    this.plan.modelo=this.modelo;
+    this.servicioPlan.crearPlan(this.plan).subscribe((data:any)=>{
+      console.log(this.plan)
     })
   }
 
