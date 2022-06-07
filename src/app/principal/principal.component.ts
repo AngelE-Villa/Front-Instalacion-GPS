373e111e72197c5888806aee4bbbdc5f43edcbfc
ismaelDjC <75846564+ismaelDjC@users.noreply.github.com>
2022-06-07 12:32:44 -0500

    implementacion de vista para escoger roles

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
 import {Router} from "@angular/router";
 import {User} from "../modelos/User";
+import {Rol} from "../modelos/Rol";
+import {Rol_Usuario} from "../modelos/Rol_Usuario";
+import {Rol_UsuarioService} from "../servicios/Rol_UsuarioService";
+import {Observable} from "rxjs";
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-principal',
@@ -9,25 +14,77 @@ import {User} from "../modelos/User";
 })
 export class PrincipalComponent implements OnInit {
 
-  constructor(private router:Router) { }
+  constructor(private router:Router,private roles_userSevice:Rol_UsuarioService,public dialog: MatDialog) { }
+  admin:boolean;
+  instalador:boolean;
+  id_persona:any;
+
+  btnUno=false;
+  btnDos=false;
+  btnTres=false;
+  btnCuatro=false;
+  btnCinco=false;
+
+  listrol=[];
+  rol_us:Rol_Usuario = new Rol_Usuario();
+
 
   issloading=false;
   usuario:User = new User();
 
+  dataSourceM:any;
+  titulo="";
+  editing=false;
+  creating=false;
+
+  @ViewChild('dialogRolesUs')
+  dialogRolesUs!: TemplateRef<any>;
+
 
   ngOnInit(): void {
-    try {
-      if(JSON.parse(sessionStorage['user']).length!=""){
-        this.usuario=JSON.parse(sessionStorage['user']);
-        this.issloading=true;
-      }else {
-        this.router.navigate(['']);
+    this.id_persona= JSON.parse(sessionStorage['id']);
+    this.roles_userSevice.getRol_Us_id_persona(this.id_persona).subscribe((data:any)=>{
+        this.listrol=data;
+        if (this.listrol.length!=0){
+          let cont=0;
+          for (let rol of this.listrol){
+            if (cont==0){
+              this.rol_us=rol;
+              cont++;
+            }
+          }
+          this.usuario=this.rol_us.usuario.nombre;
+        }
+
+      try {
+        if(this.listrol.length==1){
+          this.issloading=true;
+          this.router.navigate(['']);
+          //-- btns
+          this.btnUno=true;
+          this.btnDos=true;
+          this.btnTres=true;
+          this.btnCuatro=true;
+          this.btnCinco=true;
+          //-- btns-fin
+          this.issloading=true;
+        }else if(this.listrol.length>1){
+          this.issloading=true;
+          //Dioalogo
+          this.titulo="Seleccione su Rol"
+          this.dialog.open(this.dialogRolesUs);
+
+        }
+        else {
+          this.router.navigate(['']);
+          this.issloading=false;
+        }
+      }catch (e){
         this.issloading=false;
+        console.log("ERROR")
       }
-    }catch (e){
-      this.issloading=false;
-      console.log("ERROR")
-    }
+    });
+
 
   }
 
