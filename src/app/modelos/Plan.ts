b9583f2a91fbef46389e7c4d5453dbe3423b00c2
ismaelDjC <75846564+ismaelDjC@users.noreply.github.com>
2022-06-15 16:54:56 -0500

    cambio de metodos de palanes y vistas

--- src/app/modelos/Plan.ts
+++ src/app/modelos/Plan.ts
@@ -7,7 +7,7 @@ export class Plan {
   id_plan:any;
   descripcion_plan:any;
   estado_plan:any;
-  p_costo_mesual:any;
+  p_costo_mensual:any;
   num_descripcion_p:any;
   modelo:Modelo=new Modelo();
 }
--- src/app/modulos/seleccion-plan/seleccion-plan.component.ts
+++ src/app/modulos/seleccion-plan/seleccion-plan.component.ts
@@ -34,10 +34,17 @@ export class SeleccionPlanComponent implements OnInit {
   dialogNplan!: TemplateRef<any>;
 
   ngOnInit(): void {
+    this.listarModelo();
+    this.listarPLanes();
+  }
+
+  listarPLanes(){
     this.servicioPlan.getPlanes().subscribe((data:any)=>{
       this.listaPlanes=data;
     })
+  }
 
+  listarModelo(){
     this.modeloService.getModelos().subscribe((data:any)=>{
       this.listaModelo=data;
     })
@@ -45,17 +52,33 @@ export class SeleccionPlanComponent implements OnInit {
 
 
   abrirdialogoPlanes(){
-    this.editing=false;
-    this.creating=true;
-    this.titulo="Crear Plan"
-    this.dialog.open(this.dialogNplan);
+    this.plan=new Plan();
+      this.titulo="Crear Plan"
+    this.dialog.open(this.dialogNplan,{
+      height: '70%',
+      width: '30%',
+    });
   }
 
 
   GuardarPlan(){
+    if(this.plan.descripcion_plan=='anual'){
+      this.plan.num_descripcion_p=12;
+    }else if(this.plan.descripcion_plan=='semestral'){
+      this.plan.num_descripcion_p=6;
+    }else if(this.plan.descripcion_plan=='cuatrimestral'){
+      this.plan.num_descripcion_p=4;
+    }else if(this.plan.descripcion_plan=='trimestral'){
+      this.plan.num_descripcion_p=3;
+    }else if(this.plan.descripcion_plan=='bimestral'){
+      this.plan.num_descripcion_p=2;
+    }else{
+      this.plan.num_descripcion_p=1;
+    }
     this.plan.modelo=this.modelo;
+    console.log(this.plan);
     this.servicioPlan.crearPlan(this.plan).subscribe((data:any)=>{
-      console.log(this.plan)
+      this.listarPLanes();
     })
   }
 
